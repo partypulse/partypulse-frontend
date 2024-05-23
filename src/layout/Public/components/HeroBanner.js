88647b4dbee4f0015751e61cf53c9ce92b1ead1e
Betttina <bettina.toth@gritacademy.se>
2024-05-24 00:18:33 +0200

    added styling
    mostly focused on hero img/banner

--- src/layout/Public/components/HeroBanner.js
+++ src/layout/Public/components/HeroBanner.js
@@ -1,28 +1,29 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import heroImage from "../../../images/hero-right.png";
+import heroImage from "../../../images/balloons_banner.png";
 
 function HeroBanner() {
   return (
-    <section id="home">
+    <section id="home" className="hero-section">
       <Container>
-        <Row>
+        <Row className="hero-row">
           <div className="home-text">
             <div className="section-text__subtitle">The party never stops</div>
             <div className="section-text__title-big">
               Let's get the party started
             </div>
             <div className="section-text__body">
-              Dorem ipsum dolor sitamet, consectetur adipiscing elit, sed do
-              eiusm tempor incididunt ulabore et dolore magna aliqua.
+              Här hittar du allt du behöver för att göra ditt nästa party till
+              en succé. Vi hjälper dig att skapa minnesvärda ögonblick för alla
+              tillfällen!
             </div>
             <a href="#download" className="download-btn">
-              Shop now
+              Utforska vårt sortiment
             </a>
           </div>
 
           <div className="section-image">
-            <img src={heroImage} alt="App Preview" />
+            <img src={heroImage} alt="App Preview" className="hero-image" />
           </div>
         </Row>
       </Container>
